c36870df2f77b34d4837c6be06bf0d98472ab53e
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-05-17 16:01:19 -0500

    updated html

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 
   $(document).ready(function(){
-    getWeatherData()
 
 	});
 
@@ -10,11 +9,11 @@ function getWeatherData() {
     $.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=af44dc376863ca058191b00a4396d79b", function(data, status){
       console.log(data)
       console.log(status)
-      alert("Data: " + data + "\nStatus: " + status);
+      consolole.log("Data: " + data + "\nStatus: " + status);
     });
   }
   else {
-    alert("Please enter a city before searching.")
+   console.log("Please enter a city before searching.")
   }
 }
 
