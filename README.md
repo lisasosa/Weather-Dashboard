474bf1f39772574b9e83f1184007166ecb2b0720
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-05-19 00:00:49 -0500

    commiting final changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Weather-Dashboard
+
 Elizabeth Sosa
 
 Weather Dashboard App
@@ -8,9 +9,10 @@ https://github.com/lisasosa/Weather-Dashboard
 https://lisasosa.github.io/Weather-Dashboard/.
 
 Description:
+Weather app was built to display current and future weater data.
+Data was obtained from https://openweathermap.org/api using the 5 day / 3 hour Forecast api.
+This data pulls a total of 40 items and in ordert to get the daily data increments of 8 hours were used with data starting at 3 hurs to get data from the middle of the day.
 
-Future enhancements:
-
-Challenges:
-
-
+How the it works:
+In order to pull data you must type in a city name in the input field and then must click search.
+As you search for a different city the last entry gets logged into a list.
--- scripts.js
+++ scripts.js
@@ -1,19 +1,57 @@
+$(document).ready(function () {
 
-  $(document).ready(function(){
+  $("#search-button").click(function () {
+    console.log('searched')
+    addSearchToList()
+    getWeatherData()
+  })
 
-	});
+
+});
+
+function addSearchToList() {
+  var city = $("#city").val()
+  $("#citiesList").append("<li class='list-group-item'>" + city + "</li>")
+}
 
 function getWeatherData() {
   var city = $("#city").val();
-  if(city.length > 0) {
-    $.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=af44dc376863ca058191b00a4396d79b", function(data, status){
-      console.log(data)
-      console.log(status)
-      consolole.log("Data: " + data + "\nStatus: " + status);
-    });
-  }
-  else {
-   console.log("Please enter a city before searching.")
+  if (city.length > 0) {
+    let currentWeatherData;
+    let forecastData;
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=af44dc376863ca058191b00a4396d79b", function (data, status) {
+        currentWeatherData = data;
+      })
+      .done(function () {
+        console.log(currentWeatherData)
+      })
+      .fail(function () {
+        alert("current weather error");
+      })
+
+    $.get("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=af44dc376863ca058191b00a4396d79b", function (data, status) {
+        forecastData = data
+      }).done(function () {
+        console.log(forecastData)
+        populateFiveDay(forecastData)
+      })
+      .fail(function () {
+        alert("forecast error");
+      })
+
+
+
+  } else {
+    console.log("Please enter a city before searching.")
   }
 }
 
+function populateFiveDay(forecastData) {
+  var dayCounter = 1
+  for (var i = 3; i < forecastData["list"].length; i += 8) {
+    $("#dayTitle" + dayCounter).text(forecastData["list"][i]['dt_txt'].split(' ')[0])
+    $("#dayTemp" + dayCounter).text('Temp: ' + (forecastData["list"][i]['main']['temp']).toFixed() + ' F')
+    $("#dayHumidity" + dayCounter).text('Humidity: ' + forecastData["list"][i]['main']['humidity'] + ' %')
+    dayCounter += 1
+  }
+}
